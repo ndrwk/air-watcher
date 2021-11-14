59d7c08f24d0f6edbbe473bfc88d13877e8c68f9
drew <ndrwkalinin@gmail.com>
2021-11-14 14:11:06 +0600

    add cli

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import click
+
 from scheduler import run_scheduler
 
 
-if __name__ == '__main__':
+@click.group()
+@click.pass_context
+def main(ctx: click.core.Context) -> None:
+    """Initiate context.
+
+    """
+
+
+@main.command()
+@click.pass_context
+def runscheduler(ctx: click.core.Context) -> None:
+    """Run scheduler.
+
+    """
     run_scheduler()
+
+
+if __name__ == '__main__':
+    main()
